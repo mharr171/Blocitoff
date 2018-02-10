491f1c249bc80ea9750438b8323dd2dc7c14f624
Matthew Harris <mharr171.z@gmail.com>
2018-02-10 00:18:34 -0800

    added seeds and styled the time display

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,38 @@
+require 'faker'
+
+Faker::Internet.unique.clear
+
 # Create My Account
 my_account = User.new(
   username: 'mharr171',
-  email:      'mharr171.z@gmail.com',
-  password:   'asdf123'
+  email:    'mharr171.z@gmail.com',
+  password: 'asdf123'
 )
 my_account.skip_confirmation!
 my_account.save!
-print 'My account (mharr171.z@gmail.com) created.'
+puts '.'
 
-# Create My Account
-my_other_account = User.new(
-  username: 'mattlivefullstack',
-  email:      'matt@livefullstack.com',
-  password:   'asdf123'
-)
-my_other_account.skip_confirmation!
-my_other_account.save!
-print 'My account (matt@livefullstack.com) created.'
+10.times do
+  username = Faker::Internet.unique.user_name
+  User.create!(
+    username: username,
+    email:    Faker::Internet.safe_email(username),
+    password: 'asdf123'
+  )
+  puts '.'
+end
+users = User.all
+print "#{users.count} users created."
+
+users.each do |user|
+  5.times do
+    item = Item.new(
+      name: Faker::Lorem.sentence(3)
+    )
+    item.user = user
+    item.created_at = (rand*10).days.ago
+    puts '.' if item.save
+  end
+end
+items = Item.all
+print "#{items.count} items created."
